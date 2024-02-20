634fb607ee29625bec48ad57ddaac6f22295257c
james471 <jameswattbrh@gmail.com>
2024-02-20 19:52:06 +1100

    Add my config.py

--- common.py
+++ common.py
@@ -1,4 +1,32 @@
-FLASH_PATH = "/home/100/jw5893/flash-rsaa"
+import os
+import pickle
+from myconfig import *
+
+
+E_MAG_COLUMN_INDEX = 11
+E_KIN_COLUMN_INDEX = 9
+V_RMS_COLUMN_INDEX = 13
+TIME_COLUMN_INDEX = 0
+
+
+solverDict = {"8wave": "Split-Roe", "bouchut-split": "Split-Bouchut", "Roe": "USM-Roe", 
+              "HLLD": "USM-HLLD", "HLLC": "USM-HLLC", "bk-usm": "USM-BK"}
+colorDict = {"8wave": "#377eb8", "HLLD": "#984ea3", "HLLC": "#4daf4a", 
+              "Roe": "#a65628", "bouchut-split": "#f781bf", "bk-usm": "#ff7f00"}
+orderDict = {"8wave": 0, "bouchut-split": 1, "Roe": 2, "HLLD": 3, "HLLC": 4, "bk-usm": 5}
+
+
+def getInfoDict(sim):
+    '''
+    sim: Path to simulation directory
+    Raises exception if pkl file not found
+    '''
+    if os.path.exists(sim + "/info.pkl"):
+        with open(sim + "/info.pkl", "rb") as f:
+            return pickle.load(f)
+    else:
+        raise Exception("No info.pkl file found in " + sim)
+    
 
 def argsToOutdirName(args):
     outputDirName = (
@@ -52,3 +80,20 @@ def argsToSimulationObjectDirectory(args):
 
 
 Object = lambda **kwargs: type("Object", (), kwargs)
+
+
+def parseArgs(args, commonKeys):
+    numSim = -1
+    for key, value in vars(args).items():
+        if key not in commonKeys:
+            if value is not None and len(value) > 1:
+                numSim = len(value)
+                break
+            
+    for key, value in vars(args).items():
+        if key not in commonKeys:
+            if value is None:
+                setattr(args, key, [None for i in range(numSim)])
+            elif len(value) == 1:
+                setattr(args, key, [value[0] for i in range(numSim)])
+    return args
---
+++ myconfig.py
@@ -0,0 +1,4 @@
+#Set your flash and python flash tools paths here
+
+# FLASH_PATH = "/home/100/jw5893/flash-rsaa"
+# PYTHON_PATH = "/home/100/jw5893/flash-tools/python"
