Fix k_nu calculation for variable p_nu

spectra.py
@@ -25,8 +25,7 @@ import turblib as tl
 
 
 
-def Re_fun(k_tilde_nu, c_nu):
-    k_nu = k_tilde_nu
+def Re_fun(k_nu, c_nu):
     return (k_nu/(c_nu * 2))**(4/3)
 
 
@@ -34,11 +33,10 @@ def k_eta_fun(k_tilde_eta, p_eta):
     return (k_tilde_eta)**(1/p_eta)
 
 
-def Rm_fun(k_tilde_nu, c_nu, k_tilde_eta, p_eta, c_eta):
+def Rm_fun(k_nu, c_nu, k_tilde_eta, p_eta, c_eta):
     k_eta = k_eta_fun(k_tilde_eta, p_eta)
-    k_nu = k_tilde_nu
     Pm = (k_eta/(c_eta*k_nu))**2
-    return Pm * Re_fun(k_tilde_nu, c_nu)
+    return Pm * Re_fun(k_nu, c_nu)
 
 
 def Log10_P_kin(k, A_kin, p_bn, k_bn, k_tilde_nu, p_nu):
@@ -69,7 +67,6 @@ def generateSpectra(simDir, verbose, spectType, infoDict, lf, uf, stf, n=1):
         print("creating spectra dir: '"+spectraDir+"'")
         os.mkdir(spectraDir)
 
-    # NOTE: You made some changes here and it's different from how things were done earlier. You just removed [:101] from the end. If things break, look here first.
     files = sorted(glob.glob(simDir+"/Turb_hdf5_plt_cnt_????"))
 
     dt = infoDict['dt']
@@ -182,7 +179,7 @@ def plotSpectra(simDir, verbose, spectType, fact, infoDict, params, outdir, comp
             # cfp.plot(compensateFitFact * fact * 10**Log10_P_mag(kFit, *(fitDict["A_mag"][0], fitDict["p_mag"][0], fitDict["p_eta"][0], fitDict["k_tilde_eta"][0])), kFit, color="black")
         elif spectType == "vels":
             fitDict = loadDict(f"{outdir}/kinFitDict.pkl")[infoDict['solver']]
-            cfp.plot(compensateFitFact * fact * 10**Log10_P_kin(kFit, *(fitDict["A_kin"][0], fitDict["p_bn"][0], fitDict["k_bn"][0], fitDict["k_nu"][0]), fitDict["p_nu"][0]), kFit, color="black")
+            cfp.plot(compensateFitFact * fact * 10**Log10_P_kin(kFit, *(fitDict["A_kin"][0], fitDict["p_bn"][0], fitDict["k_bn"][0], fitDict["k_tilde"][0]), fitDict["p_nu"][0]), kFit, color="black")
         elif spectType == "cur":
             fitDict = loadDict(f"{outdir}/curFitDict.pkl")[infoDict['solver']]
 
@@ -236,7 +233,7 @@ def fit_func(spectType, simDir, kFit, log10PTotFit, deltaLog10PTotFit, params, c
         fitDict["A_kin"] = (kinFit.popt[0], kinFit.perr[0][0], kinFit.perr[0][1])
         fitDict["p_bn"] = (kinFit.popt[1], kinFit.perr[1][0], kinFit.perr[1][1])
         fitDict["k_bn"] = (kinFit.popt[2], kinFit.perr[2][0], kinFit.perr[2][1])
-        fitDict["k_nu"] = (kinFit.popt[3], kinFit.perr[3][0], kinFit.perr[3][1])
+        fitDict["k_tilde_nu"] = (kinFit.popt[3], kinFit.perr[3][0], kinFit.perr[3][1])
         fitDict["p_nu"] = (kinFit.popt[4], kinFit.perr[4][0], kinFit.perr[4][1])
     
     return fitDict
@@ -279,7 +276,7 @@ def plotScaleLoc(plObj, solverFit, type):
         maxKNu = 0
         for solver in solverFit:
             val = solverFit[solver]
-            k_nu = val["k_nu"][0]
+            k_nu = val["k_tilde_nu"][0]**(1/val["p_nu"][0])
             ax.plot([k_nu, k_nu], [ylim[0], 2*ylim[0]], color=COLOR_DICT[solver], scaley=False)
             if k_nu > maxKNu:
                 maxKNu = k_nu
@@ -318,7 +315,7 @@ def main(args):
             fit = True
             if not args.refit and os.path.exists(f"{args.o}/kinFitDict.pkl"):
                 fit = False
-            kinParams = {"A_kin": [0, 0.0015, np.inf], "p_bn": [0, 1, np.inf], "k_bn": [0.1, 4.0, 128], "k_tilde_nu": [0.1, 4.0, 128], "p_nu": [1, 1, 1+1e-15]}
+            kinParams = {"A_kin": [0, 0.0015, np.inf], "p_bn": [0, 1, np.inf], "k_bn": [0.1, 4.0, 128], "k_tilde_nu": [0.1, 4.0, 128], "p_nu": [1, 1, 1+1e-6]}
             if os.path.exists(simDir+"/spectra/kinFitInit.txt"):
                 kinParams = txtToCfpDict(simDir+"/spectra/kinFitInit.txt")
                 print("Using kin dict:", kinParams)